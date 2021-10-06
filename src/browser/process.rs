7006ca53eca66e751a8b9609dd499f3e94db1689
dhkim <dhkim93@monitorapp.com>
2021-10-06 12:36:35 +0900

    add not zygote flag

--- src/browser/process.rs
+++ src/browser/process.rs
@@ -161,7 +161,7 @@ impl<'a> LaunchOptions<'a> {
 
 /// These are passed to the Chrome binary by default.
 /// Via https://github.com/GoogleChrome/puppeteer/blob/master/lib/Launcher.js#L38
-static DEFAULT_ARGS: [&str; 24] = [
+static DEFAULT_ARGS: [&str; 25] = [
     "--disable-background-networking",
     "--enable-features=NetworkService,NetworkServiceInProcess",
     "--disable-background-timer-throttling",
@@ -187,6 +187,7 @@ static DEFAULT_ARGS: [&str; 24] = [
     "--enable-automation",
     "--password-store=basic",
     "--use-mock-keychain",
+    "--no-zygote"
 ];
 
 impl Process {
